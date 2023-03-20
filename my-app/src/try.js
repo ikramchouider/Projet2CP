aa04f02d037c44c97f7cba6181a5366f75f0118e
Rania Bellili <91795526+Rbellili@users.noreply.github.com>
2023-03-20 23:51:19 +0100

    Create try.js

---
+++ my-app/src/try.js
@@ -0,0 +1,15 @@
+import React   from 'react';
+class RI {
+    remplirRi = (code) => {
+    cop = code.substring(0, 5);
+    ma  = code.substring(6, 7);
+    f  = code.substring(8, 8);
+    d  = code.substring(9 , 9);
+    reg1 = code.substring(10,12);
+    reg2 = code.substring(13,15);
+    }
+decodage = () => {
+
+}
+   
+  }
