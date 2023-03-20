1c0a9f44f348b8bc75068bf9429e720c6d8b22cc
Rania Bellili <lr_bellili@esi.dz>
2023-03-21 00:17:30 +0100

    ggg

---
+++ my-app/src/try.js
@@ -0,0 +1,23 @@
+/*
+class RI {
+    remplirRI = (code) => {
+    cop = code.substring(0, 5);
+    ma  = code.substring(6, 7);
+    f  = code.substring(8, 8);
+    d  = code.substring(9 , 9);
+    reg1 = code.substring(10,12);
+    reg2 = code.substring(13,15);
+    }
+trouverMA =(ma) => {
+    if (ma == "00") { console.log("le mode d'adressage est ")}
+}
+ decodage = () => {
+
+}
+   
+  }*/
+  let y='yasmine';
+  console.log(y);
+
+  
+  
