24d731fb1e30a1baf3b26716fcb8da32decd1f88
Karou-Yasmine <karouyasmine.pro@gmail.com>
2023-03-24 12:48:39 +0100

    rien d'important

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -5,7 +5,7 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+root.render( //Renvoie le code html ecrit dans App.js
   <React.StrictMode>
     <App />
   </React.StrictMode>
--- my-app/src/try.js
+++ my-app/src/try.js
@@ -1,4 +1,16 @@
+
+
 class RI {
+  
+  constructor () {
+    this.cop = null ;
+    this.ma = null ;
+    this.f = null ;
+    this.d = null ;
+    this.reg1 = null;
+    this.reg2 = null ;
+  } 
+
     remplirRI = (code) => {
     cop = code.substring(0, 5);
     ma  = code.substring(6, 7);
@@ -8,7 +20,7 @@ class RI {
     reg2 = code.substring(13,15);
     }
 
-trouverMa =(ma) => {
+trouverMa = (ma) => {
   let mode ;
   switch (ma) {
     case "00" : mode = 'Direct' ;
@@ -19,55 +31,57 @@ trouverMa =(ma) => {
   console.log ("Le mode d'adressage est: "+mode) ;
 
 }
-trouverDest (d) => {
+trouverDest = (d) => {
   if (d==0) {
 
   }
 }
-trouverCop =(cop) => {
-    if (cop.substring(0, 0) == '0') { 
-  let ins;
-  switch (cop.substring(1, 5)) 
-  {case "00000" : ins = 'MOV';
-   case "00001" : ins = 'ADD';
-   case "00010" : ins = 'ADA';
-   case "00011" : ins = 'SUB';
-   case "00100" : ins = 'SUBA';
-   case "00101" : ins = 'CMP';
-   case "00110" : ins = 'OR';
-   case "00111" : ins = 'AND';
-   case "01000" : ins = 'SHR';
-   case "01001" : ins = 'SHL';
-   case "01010" : ins = 'ROL';
-   case "01011" : ins = 'ROR';
-   case "01100" : ins = 'JZ';
-   case "01101" : ins = 'JNZ';
-   case "01110" : ins = 'JC';
-   case "01111" : ins = 'JNC';
-   case "10000" : ins = 'JS';
-   case "10001" : ins = 'JNS';
-   case "10010" : ins = 'JO';
-   case "10011" : ins = 'JNO';
-   case "10100" : ins = 'JE';
-   case "10101" : ins = 'JNE';
-   case "10110" : ins = 'LOAD';
-   case "10111" : ins = 'STORE';
-   case "11000" : ins = 'INC';
-   case "11001" : ins = 'DEC';
-   case "11010" : ins = 'NOT';
-   case "11011" : ins = 'JMP';
-   case "11100" : ins = 'IN';
-   case "11101" : ins = 'OUT';
-   case "11110" : ins = 'STOP';
+trouverCop = (cop) => {
+  var ins ;
+  if (cop.substring(0, 1) == '0') {
+  switch (cop.substring(1, 6)) 
+  {case "00000" : ins = 'MOV'; break ;       
+   case "00001" : ins = 'ADD'; break ;
+   case "00010" : ins = 'ADA'; break ;
+   case "00011" : ins = 'SUB'; break ;
+   case "00100" : ins = 'SUBA'; break ;
+   case "00101" : ins = 'CMP'; break ;
+   case "00110" : ins = 'OR'; break ;
+   case "00111" : ins = 'AND'; break ;
+   case "01000" : ins = 'SHR'; break ;
+   case "01001" : ins = 'SHL'; break ;
+   case "01010" : ins = 'ROL'; break ;
+   case "01011" : ins = 'ROR'; break ;
+   case "01100" : ins = 'JZ'; break ;
+   case "01101" : ins = 'JNZ'; break ;
+   case "01110" : ins = 'JC'; break ;
+   case "01111" : ins = 'JNC'; break ;
+   case "10000" : ins = 'JS'; break ;
+   case "10001" : ins = 'JNS'; break ;
+   case "10010" : ins = 'JO'; break ; 
+   case "10011" : ins = 'JNO'; break ; 
+   case "10100" : ins = 'JE'; break ; 
+   case "10101" : ins = 'JNE'; break ; 
+   case "10110" : ins = 'LOAD'; break ; 
+   case "10111" : ins = 'STORE'; break ;
+   case "11000" : ins = 'INC'; break ; 
+   case "11001" : ins = 'DEC'; break ;
+   case "11010" : ins = 'NOT'; break ;
+   case "11011" : ins = 'JMP'; break ;
+   case "11100" : ins = 'IN'; break ;
+   case "11101" : ins = 'OUT'; break ; 
+   case "11110" : ins = 'STOP'; break ; 
   }
     }
-    else {
-
-    }
-    console.log(ins);
+    else {}
+    
+  return ins ;
 }
- decodage = () => {
+} ;
 
-}
-   
-  }
+
+  var ri = new RI() ;
+  console.log(ri.trouverCop('000001'));
+
+
+
