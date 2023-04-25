dd9e3668ff796beb74cb843764905a3e0c95e371
unknown <ln_rahal@esi.dz>
2023-04-25 21:35:38 +0200

    ual.js

--- my-app/backend/classes/ual.js
+++ my-app/backend/classes/ual.js
@@ -1,6 +1,7 @@
 import main from "./main.js";
 import util from "./util.js";
 import registre from "./registre.js";
+import CaseMc from "./caseMemoire.js";
 class UAL {
   #eUal1;
   #eUal2;
@@ -8,7 +9,7 @@ class UAL {
     this.#eUal1 = eUal1;
     this.#eUal2 = eUal2;
   }
-  add = function (dataTab, modeAdr,cpt, dest, format, param1, param2) {
+  add = function (dataTab,instrTab,modeAdr,cpt, dest, format, param1, param2) {
     //addition registre registre et le resultat sera dans reg1 000000 00 0 1 REG1   REG2
     let somme = 0;
     if ((modeAdr = "00") && dest == "1" && format == "0") {
@@ -65,6 +66,57 @@ class UAL {
     } else {
       if (modeAdr == "00" && format == "1") {
         //mode d'adr direct et l'instruc sur deux mot
+          let m=0 ;
+          let i = util.chercherAdr(dataTab,(instrTab[cpt+1].getVal()).slice(1)) ;
+             if(i==dataTab.length) {dataTab.push(new CaseMc((instrTab[cpt+1].getVal()).slice(1),0,""));
+             } 
+             m = dataTab[i].getVal() ; 
+           if(dest == "0"){
+            switch (param1) {
+              case "000": m = main.AX.getContenu(); break;
+              case "001": m = main.BX.getContenu(); break;
+              case "010": m = main.CX.getContenu(); break;
+              case "011": m = main.DX.getContenu(); break;
+              case "100": m = main.EX.getContenu(); break;
+              case "101": m = main.FX.getContenu(); break;
+            }
+          
+            
+          dataTab[i].setVal(util.remplirZero(util.additionHexa(dataTab[i].getVal(),m),4,0),m) ; 
+          return cpt+2
+           } 
+            else {
+              let n=0 ; 
+              switch (param1) {
+                case "000":
+                  n = main.AX.getContenu();
+                  main.AX.setContenu(util.remplirZero(util.additionHexa(n,m),4,0));
+                  break;
+                case "001":
+                  n = main.BX.getContenu();
+                  main.BX.setContenu(util.remplirZero(util.additionHexa(n,m),4,0));
+                  break;
+                case "010":
+                  n = main.CX.getContenu();
+                  main.CX.setContenu(util.remplirZero(util.additionHexa(n,m),4,0));
+                  break;
+                case "011":
+                  n = main.DX.getContenu();
+                  main.DX.setContenu(util.remplirZero(util.additionHexa(n,m),4,0));
+                  break;
+                case "100":
+                  n = main.EX.getContenu();
+                  main.EX.setContenu(util.remplirZero(util.additionHexa(n,m),4,0));
+                  break;
+                case "101":
+                  n = main.FX.getContenu();
+                  main.FX.setContenu(util.remplirZero(util.additionHexa(n,m),4,0));
+                  break;
+              }
+              
+             
+           }
+
       }
     }
   };
