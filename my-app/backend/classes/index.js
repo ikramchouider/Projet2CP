7b4b87af914b2e92dd274df89353cba1c7fc538b
khawla <lk_bouguerra@esi.dz>
2023-05-06 21:42:42 +0100

    simulation

---
+++ my-app/backend/classes/index.js
@@ -0,0 +1,7 @@
+
+import assembler from "./assembler.js";
+ assembler.errorFunction("test.txt");
+ //let tab = new Array();
+// tab = assembler.errorFunction("test.txt"); 
+ //console.log(tab[0]); 
+ //document.getElementById("aaa").innerHTML= assembler.errorFunction("./test.txt");
